d12b39d93027ead64932f8fa62f387bce5b776aa
AnteSocial <39884930+AnteSocial@users.noreply.github.com>
2018-06-05 19:25:20 +0100

    Basic files

---
+++ script.js
@@ -0,0 +1,39 @@
+function a(id){return document.getElementById(id)};
+var game = {
+	replicanti: {
+		display: a("replicanti"),
+		amount: new Decimal(1),
+		chance: new Decimal(10)
+	},
+	updateHTML: function(){
+		game.replicanti.display.innerHTML = game.replicanti.amount.floor();
+	},
+	replicantiUpdate: function(){
+		window.setInterval(function(){
+			if(game.replicanti.amount.lte(100)){
+				console.log("init")
+				for(i=0;game.replicanti.amount.lt(i);i++){
+					console.log("hi")
+					if(game.replicanti.chance.lt(Math.random()*100)){
+						console.log("added")
+						game.replicanti.amount.plus(1);
+						game.updateHTML()
+					}
+				}
+			} else if(game.replicanti.amount.gte(100)) {
+				let x = 0
+				for(i=0;i<100;i++){
+					if(game.replicanti.chance.lt(Math.random()*100)){
+						x++
+					}
+				}
+				game.replicanti.amount = game.replicanti.amount.mul(1+x/100).floor()
+				game.updateHTML()
+			}
+		},1000)
+	}	
+}
+
+game.replicantiUpdate()
+
+console.log(game.replicanti.amount)
