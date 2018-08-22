3c14774c62ee4ab1afd23205480fad0c2892eeb4
btalbot16 <39996736+btalbot16@users.noreply.github.com>
2018-08-22 15:53:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ document.getElementById("dogfact").innerHTML = dog_facts[Math.floor(Math.random(
 document.getElementById("click").onclick = function(){
     document.getElementById("dogfact").innerHTML = dog_facts[Math.floor(Math.random() * (27 - 1 + 1)) + 1 - 1]
 }
+
+var checked = false;
+
+window.onload = function(){
+    alert("For I am a sinner in the hands of an angry God. Bloody Mary, full of vodka, blessed are you among cocktails. Pray for me now and at the hour of my death, which I hope is soon. Amen.")
+    checked = true;
+}
